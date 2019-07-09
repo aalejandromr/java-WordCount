eac6369d962af111a290f8d486a8541a3a6ec2b2
Alejandro Mejia <aalejandro.mreyes@icloud.com>
2019-07-09 18:34:47 -0400

    Gets Stretch Goal Done

--- src/javaWordCount/Main.java
+++ src/javaWordCount/Main.java
@@ -2,7 +2,7 @@ package javaWordCount;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.HashMap;
+import java.util.*;
 
 public class Main
 {
@@ -24,11 +24,34 @@ public class Main
         wBw.put(w, 1);
       }
     }
-    for(String word : wBw.keySet())
+    // for(String word : wBw.keySet())
+    // {
+    //   System.out.println("key: " + word);
+    //   System.out.println("Value: " + wBw.get(word));
+    // }
+
+    ArrayList<HashMap.Entry<String, Integer>> sortedWords = new ArrayList<>();
+    sortedWords.addAll(wBw.entrySet());
+    Collections.sort(sortedWords, new Comparator<HashMap.Entry<String, Integer>>(){
+      public int compare(HashMap.Entry<String, Integer> o1, HashMap.Entry<String, Integer> o2)
+      {
+        return o2.getValue() - o1.getValue();
+      }
+    });
+
+    // for (HashMap.Entry<String, Integer> d : sortedWords)
+		// {
+		// 	System.out.println("key: " + d.getKey() + " value: " + d.getValue());
+    // }
+    
+    for(int i = 0; i < 49; i++)
     {
-      System.out.println("key: " + word);
-      System.out.println("Value: " + wBw.get(word));
+      Map.Entry<String,Integer> entry = new AbstractMap.SimpleEntry<String, Integer>(sortedWords.get(i));
+      System.out.println("'" + entry.getKey() + "'" + " was found " + entry.getValue() + " times.");
     }
+
+    // Array -> Collection -> ArrayList -> List
+
   }
 
   private static String readAllBytes(String filePath)
